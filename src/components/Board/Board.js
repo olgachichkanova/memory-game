79b7f2e210b7847c6a1624032458aeccc8d913fd
Olga Andriychenko <olga@glasnostic.com>
2023-12-19 15:02:26 +0800

    added shaffle cards function

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,116 +1,11 @@
 import React from 'react';
 import { Card } from '../Card/Card.js'
 import './Board.css';
-import twoImg from '../../images/2.svg';
-import threeImg from '../../images/3.svg';
-import nginxImg from '../../images/nginx.svg';
-import reactImg from '../../images/react.svg';
-import wsImg from '../../images/ws.svg';
-import jsImg from '../../images/js.svg';
-import webpackImg from '../../images/webpack.svg';
-import tsImg from '../../images/ts.svg';
-
+import { randomCards } from '../../utils.js';
 
 export const Board = ({onCardClick}) => {
-  const placeholder = [
-      {
-          id: 'card1',
-          img: twoImg,
-          name: 'twoImg',
-          isOpen: false
-      },
-      {
-          id: 'card2',
-          img: twoImg,
-          name: 'twoImg',
-          isOpen: false
-      },
-      {
-          id: 'card3',
-          img: threeImg,
-          name: 'threeImg',
-          isOpen: false
-      },
-      {
-          id: 'card4',
-          img: threeImg,
-          name: 'threeImg',
-          isOpen: false
-      },
-      {
-          id: 'card5',
-          img: nginxImg,
-          name: 'nginxImg',
-          isOpen: false
-      },
-      {
-          id: 'card6',
-          img: nginxImg,
-          name: 'nginxImg',
-          isOpen: false
-      },
-      {
-          id: 'card7',
-          img: reactImg,
-          name: 'reactImg',
-          isOpen: false
-      },
-      {
-          id: 'card8',
-          img: reactImg,
-          name: 'reactImg',
-          isOpen: false
-      },
-      {
-          id: 'card9',
-          img: wsImg,
-          name: 'wsImg',
-          isOpen: false
-      },
-      {
-          id: 'card10',
-          img: wsImg,
-          name: 'wsImg',
-          isOpen: false
-      },
-      {
-          id: 'card11',
-          img: jsImg,
-          name: 'jsImg',
-          isOpen: false
-      },
-      {
-          id: 'card12',
-          img: jsImg,
-          name: 'jsImg',
-          isOpen: false
-      },
-      {
-          id: 'card13',
-          img: webpackImg,
-          name: 'webpackImg',
-          isOpen: false
-      },
-      {
-          id: 'card14',
-          img: webpackImg,
-          name: 'webpackImg',
-          isOpen: false
-      },
-      {
-          id: 'card15',
-          img: tsImg,
-          name: 'tsImg',
-          isOpen: false
-      },
-      {
-          id: 'card16',
-          img: tsImg,
-          name: 'tsImg',
-          isOpen: false
-      }
-  ];
-  const [cards, setCards] = React.useState(placeholder);
+  
+  const [cards, setCards] = React.useState(randomCards);
   const [openCards, setOpenCards] = React.useState([]);
 
   const handleCardClick = (id) => {
@@ -121,7 +16,7 @@ export const Board = ({onCardClick}) => {
       )
     );
 
-    const clickedCard = placeholder.find((card) => card.id === id);
+    const clickedCard = randomCards.find((card) => card.id === id);
     setOpenCards((prevOpenCards) =>
       prevOpenCards.some((card) => card.id === clickedCard.id)
         ? prevOpenCards
---
+++ src/utils.js
@@ -0,0 +1,116 @@
+import twoImg from '../src/images/2.svg';
+import threeImg from '../src/images/3.svg';
+import nginxImg from '../src/images/nginx.svg';
+import reactImg from '../src/images/react.svg';
+import wsImg from '../src/images/ws.svg';
+import jsImg from '../src/images/js.svg';
+import webpackImg from '../src/images/webpack.svg';
+import tsImg from '../src/images/ts.svg';
+
+export const cards = [
+    {
+        id: 'card1',
+        img: twoImg,
+        name: 'twoImg',
+        isOpen: false
+    },
+    {
+        id: 'card2',
+        img: twoImg,
+        name: 'twoImg',
+        isOpen: false
+    },
+    {
+        id: 'card3',
+        img: threeImg,
+        name: 'threeImg',
+        isOpen: false
+    },
+    {
+        id: 'card4',
+        img: threeImg,
+        name: 'threeImg',
+        isOpen: false
+    },
+    {
+        id: 'card5',
+        img: nginxImg,
+        name: 'nginxImg',
+        isOpen: false
+    },
+    {
+        id: 'card6',
+        img: nginxImg,
+        name: 'nginxImg',
+        isOpen: false
+    },
+    {
+        id: 'card7',
+        img: reactImg,
+        name: 'reactImg',
+        isOpen: false
+    },
+    {
+        id: 'card8',
+        img: reactImg,
+        name: 'reactImg',
+        isOpen: false
+    },
+    {
+        id: 'card9',
+        img: wsImg,
+        name: 'wsImg',
+        isOpen: false
+    },
+    {
+        id: 'card10',
+        img: wsImg,
+        name: 'wsImg',
+        isOpen: false
+    },
+    {
+        id: 'card11',
+        img: jsImg,
+        name: 'jsImg',
+        isOpen: false
+    },
+    {
+        id: 'card12',
+        img: jsImg,
+        name: 'jsImg',
+        isOpen: false
+    },
+    {
+        id: 'card13',
+        img: webpackImg,
+        name: 'webpackImg',
+        isOpen: false
+    },
+    {
+        id: 'card14',
+        img: webpackImg,
+        name: 'webpackImg',
+        isOpen: false
+    },
+    {
+        id: 'card15',
+        img: tsImg,
+        name: 'tsImg',
+        isOpen: false
+    },
+    {
+        id: 'card16',
+        img: tsImg,
+        name: 'tsImg',
+        isOpen: false
+    }
+];
+
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
+export const randomCards = shuffleArray([...cards]);
