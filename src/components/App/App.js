03accbff67ec64fcec4d44661cb9761e8dc8156a
Olga Andriychenko <olga@glasnostic.com>
2023-12-19 19:22:24 +0800

    added results

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,16 +6,45 @@ import './App.css';
 export const App = () => {
     const [counter, setCounter] = React.useState(0);
     const totalSteps = 40;
+    const [isWinner, setIsWinner] = React.useState(false);
+    const [isLooser, setIsLooser] = React.useState(false);
+    const [isResetGame, setIsResetGame] = React.useState(false);
 
     const handleCardClick = () => {
         setCounter((prevCounter) => prevCounter + 1);
     };
+    const handleReset = (value) => {
+        setIsResetGame(value);
+        setCounter(0);
+        setIsLooser(false)
+        setIsWinner(false)
+    }
+    const handleGameWon = () => setIsWinner(true);
+    React.useEffect(() => {
+        if(counter === 40 && !isWinner) {
+            setIsLooser(true)
+        }
+      }, [isLooser, isWinner, counter, isResetGame]);
     return (
         <section className='board'>
             <Steps 
                 title='Сделано ходов'
                 count={counter}/>
-            <Board onCardClick={handleCardClick}/>
+            {isLooser && 
+            <div className='results-wrapper'>
+                <div className="results">
+                    <h2>Loooooooser!</h2>
+                    <button className='button' onClick={() => handleReset(true)}>Play again</button>
+                </div>
+            </div>}
+            {isWinner === true && <div className='results-wrapper'>
+                <div className="results">
+                    <h2>Congratulations!</h2>
+                    <p>You won with ${counter} steps.</p>
+                    <button className='button' onClick={() => handleReset(true)}>Play again</button>
+                </div>
+            </div>}
+            <Board onCardClick={handleCardClick} onGameWon={handleGameWon} isReset={isResetGame} onReset={(value) => handleReset(value)}/>
             <Steps 
                 title='Осталось ходов'
                 count={totalSteps - counter}/>
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -3,19 +3,20 @@ import { Card } from '../Card/Card.js'
 import './Board.css';
 import { randomCards } from '../../utils.js';
 
-export const Board = ({onCardClick}) => {
+export const Board = ({onCardClick, onGameWon, isReset, onReset}) => {
   
   const [cards, setCards] = React.useState(randomCards);
   const [openCards, setOpenCards] = React.useState([]);
 
-  const handleCardClick = (id) => {
-    onCardClick();
+  const updateCards = (id) => {
     setCards((prevCards) =>
       prevCards.map((card) =>
         card.id === id ? { ...card, isOpen: !card.isOpen } : card
       )
     );
-
+  };
+  
+  const updateOpenCards = (id) => {
     const clickedCard = randomCards.find((card) => card.id === id);
     setOpenCards((prevOpenCards) =>
       prevOpenCards.some((card) => card.id === clickedCard.id)
@@ -23,34 +24,63 @@ export const Board = ({onCardClick}) => {
         : [...prevOpenCards, clickedCard]
     );
   };
-
-  React.useEffect(() => {
-    if (openCards.length > 2) {
-      const lastClickedCardId = openCards[openCards.length - 1].id;
+  
+  const closeUnmatchedCards = (lastClickedCardId) => {
+    setCards((prevCards) =>
+      prevCards.map((card) =>
+        openCards.some((openCard) => openCard.id === card.id) &&
+        card.id !== lastClickedCardId
+          ? { ...card, isOpen: false }
+          : card
+      )
+    );
+    setOpenCards((prevOpenCards) => [prevOpenCards[prevOpenCards.length - 1]]);
+  };
+  
+  const removeMatchedCards = () => {
+    setTimeout(() => {
       setCards((prevCards) =>
         prevCards.map((card) =>
-          openCards.some((openCard) => openCard.id === card.id) && card.id !== lastClickedCardId
-            ? { ...card, isOpen: false }
+          openCards.some((openCard) => openCard.id === card.id)
+            ? { ...card, img: null, isOpen: false }
             : card
         )
       );
-    
-      setOpenCards((prevOpenCards) => [prevOpenCards[prevOpenCards.length - 1]]);
-    }
+    }, 500);
+  };
 
+  const allCardsMatched = () => {
+    return cards.every((card) => card.img === null);
+  };
+
+  const resetGame = () => {
+    setOpenCards([]);
+    setCards(randomCards);
+    onReset(false);
+  };
+  
+  const handleCardClick = (id) => {
+    onCardClick();
+    updateCards(id);
+    updateOpenCards(id);
+  };
+  
+  React.useEffect(() => {
+    if(isReset){
+      resetGame();
+    }
+    if (openCards.length > 2) {
+      const lastClickedCardId = openCards[openCards.length - 1].id;
+      closeUnmatchedCards(lastClickedCardId);
+    }
+  
     if (openCards.length === 2 && openCards[0].name === openCards[1].name) {
-      setTimeout(() => {
-        setCards((prevCards) =>
-          prevCards.map((card) =>
-            openCards.some((openCard) => openCard.id === card.id)
-              ? { ...card, img: null, isOpen: false }
-              : card
-          )
-        );
-      }, 500);
+      removeMatchedCards();
+      if (allCardsMatched()) {
+        onGameWon();
+      }
     }
-  }, [cards, openCards]);
-
+  }, [cards, openCards, isReset]);  
   return (
     <div className="cards-wrapper">
       <h1 className="header">Memory</h1>
