d712af2714c9c5c62fc43e06f65aad52d0a4acf5
Olga Andriychenko <olga@glasnostic.com>
2023-12-23 19:19:11 +0800

    added rusult component file

---
+++ src/components/Result/Result.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+import './Result.css';
+
+interface Props {
+    isWinner: boolean;
+    counter: number;
+    resetGame: () => void;
+}
+
+export const Result: FC<Props> = ({isWinner, counter, resetGame}) => {
+    return (
+        <div className='results-wrapper'>
+            <div className="results">
+                <h2>{isWinner ? 'Congratulations!' : 'Loooooser'}</h2>
+                <p>{isWinner ? `You won with ${counter} steps.` : ''}</p>
+                <button className='button' onClick={() => resetGame()}>Play again</button>
+            </div>
+        </div>
+    )
+}
