3276e045d78a665e62411b1c5b6c2e02b6bb3655
Olga Andriychenko <olga@glasnostic.com>
2023-12-29 17:09:30 +0800

    added linters, formated code

--- src/components/Steps/Steps.tsx
+++ src/components/Steps/Steps.tsx
@@ -2,15 +2,15 @@ import React, { FC } from 'react';
 import './Steps.css';
 
 interface Props {
-    title: string;
-    count: number;
+  title: string;
+  count: number;
 }
 
-export const Steps: FC<Props> = ({title, count}) => {
-    return (
-        <div className="steps">
-            <h2>{title}</h2>
-            <p>{count}</p>
-        </div>
-    )
-}
+export const Steps: FC<Props> = ({ title, count }) => {
+  return (
+    <div className="steps">
+      <h2>{title}</h2>
+      <p>{count}</p>
+    </div>
+  );
+};
